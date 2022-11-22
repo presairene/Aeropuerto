1c129bcdc4e1609ce4b88583271d45216f0517cc
presairene <117373887+presairene@users.noreply.github.com>
2022-11-22 15:27:23 +0100

    Update CDatabaseLab4.hpp

--- smart-systems-template-proj/src/dm-database/CDatabaseLab4.hpp
+++ smart-systems-template-proj/src/dm-database/CDatabaseLab4.hpp
@@ -14,8 +14,27 @@ public:
 	~CDatabaseLab4();
 
 	int insertLocalizacion(const CLocalizacion& c);
+<<<<<<< Updated upstream
 	
+=======
+
+	int insertRemolque(const CRemolque& r);
+	int insertAvion(const CAvion& a);
+	int insertPrediccion(const CSensor& s, const CPrediccion& p);
+
+	int insertSensor(const CSensor& s);
+	int insertSensorBateria(const CSensor& s, const CRemolque& r);
+	int insertSensorLocalizacion(const CSensor& s, const CLocalizacion& l);
+	int insertTipo(const CTipo& t);
+	int insertValor(const CValue& v, const CSensor& s);
+
+	//OTHER
+	int EnviarRemolquesCarga();
+	int LeerSensorLocFINGER();
+	int LeerSensorLocPISTA();
+	int CambiarEstadoAvion();
+>>>>>>> Stashed changes
 
 private:
-
-};
+}
+;
